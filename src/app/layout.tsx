33bcb4c592180a79490957ab7c16c4d7b4d9fada
Eric Dai <ericdai2000@gmail.com>
2025-01-04 02:38:45 -0500

    test2 opengraph

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,26 +7,26 @@ export const metadata: Metadata = {
   title: 'Eric Dai',
   description:
     'I create interface tools that amplify human understanding and creative expression. I am a candidate for MSE in Computer Science at Penn. I help others design and build UI under my studio Ren. Previously, I prototyped software at Konrad Group, and devised strategy at Adobe.',
-  openGraph: {
-    title: 'Eric Dai',
-    description:
-      'I create tools for thought - interfaces that amplify human understanding and creative expression. I am a candidate for MSE in Computer Science at Penn. I help others design and build UI under my studio Ren. Previously, I prototyped software at Konrad Group, and devised strategy at Adobe.',
-    images: [
-      {
-        url: '/meta-pic.png',
-        width: 1200,
-        height: 630,
-        alt: 'Eric Dai - Portfolio',
-      },
-    ],
-  },
-  twitter: {
-    card: 'summary_large_image',
-    title: 'Eric Dai',
-    description:
-      'I create tools for thought - interfaces that amplify human understanding and creative expression. I am a candidate for MSE in Computer Science at Penn. I help others design and build UI under my studio Ren. Previously, I prototyped software at Konrad Group, and devised strategy at Adobe.',
-    images: ['/meta-pic.png'],
-  },
+  // openGraph: {
+  //   title: 'Eric Dai',
+  //   description:
+  //     'I create tools for thought - interfaces that amplify human understanding and creative expression. I am a candidate for MSE in Computer Science at Penn. I help others design and build UI under my studio Ren. Previously, I prototyped software at Konrad Group, and devised strategy at Adobe.',
+  //   images: [
+  //     {
+  //       url: '/opengraph-image.png',
+  //       width: 1200,
+  //       height: 630,
+  //       alt: 'Eric Dai - Portfolio',
+  //     },
+  //   ],
+  // },
+  // twitter: {
+  //   card: 'summary_large_image',
+  //   title: 'Eric Dai',
+  //   description:
+  //     'I create tools for thought - interfaces that amplify human understanding and creative expression. I am a candidate for MSE in Computer Science at Penn. I help others design and build UI under my studio Ren. Previously, I prototyped software at Konrad Group, and devised strategy at Adobe.',
+  //   images: ['/twitter-image.png'],
+  // },
 };
 
 export default function RootLayout({
@@ -40,6 +40,8 @@ export default function RootLayout({
       className={`${gambetta.variable} ${GeistSans.variable} ${switzer.variable} antialiased`}
     >
       <body>{children}</body>
+      <meta property="og:image" content="/opengraph-image.png" />
+      <meta property="twitter:image" content="/twitter-image.png" />
     </html>
   );
 }
