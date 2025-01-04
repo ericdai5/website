6711b6dcede3ce4f6d9b58ab1d7ccaf37330d6cd
Eric Dai <ericdai2000@gmail.com>
2025-01-04 03:13:05 -0500

    suppressed hydration warning

--- src/components/LiveTime.tsx
+++ src/components/LiveTime.tsx
@@ -7,10 +7,8 @@ interface LiveTimeProps {
 
 export default function LiveTime({ timezone = 'America/New_York' }: LiveTimeProps) {
   const [time, setTime] = useState<string>('');
-  const [mounted, setMounted] = useState(false);
 
   useEffect(() => {
-    setMounted(true);
     const updateTime = () => {
       const formatter = new Intl.DateTimeFormat('en-US', {
         timeZone: timezone,
@@ -21,25 +19,17 @@ export default function LiveTime({ timezone = 'America/New_York' }: LiveTimeProp
       });
       setTime(formatter.format(new Date()));
     };
-    // Update immediately
+
     updateTime();
-    // Update every second
     const interval = setInterval(updateTime, 1000);
-    // Cleanup on unmount
     return () => clearInterval(interval);
   }, [timezone]);
 
-  // Return empty string on server-side or during initial client render
-  if (!mounted) {
-    return (
-      <span className="font-geist text-[18px] font-[350] tracking-[-0.01em] text-[var(--text-60)]">
-        --:--:-- --
-      </span>
-    );
-  }
-
   return (
-    <span className="font-geist text-[18px] font-[350] tracking-[-0.01em] text-[var(--text-60)]">
+    <span
+      suppressHydrationWarning
+      className="font-geist text-[18px] font-[350] tracking-[-0.01em] text-[var(--text-60)]"
+    >
       {time}
     </span>
   );
